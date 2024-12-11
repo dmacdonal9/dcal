d2b2c67f2764b10d3c01584af7dcd5b9d8150e70
darcymac <darcymac@gmail.com>
2024-12-11 09:04:02 -0400

    adding orderRef tag support

--- cfg.py
+++ cfg.py
@@ -8,7 +8,7 @@ target_call_delta = 15
 
 # IBKR Connection Parameters
 ib_host = '127.0.0.1'
-ib_port = 7500  # Port should be an integer
+ib_port = 7496  # Port should be an integer
 ib_clientid = 1  # Client ID should also be an integer
 
 short_expiry_days = 1
