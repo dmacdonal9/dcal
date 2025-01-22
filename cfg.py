94aa647a122b8ba6aad0a6d730ec52ec65b2d9df
darcymac <darcymac@gmail.com>
2025-01-22 15:25:59 -0400

    fixes

--- cfg.py
+++ cfg.py
@@ -3,7 +3,6 @@ fri_57dc_symbols = ['SPX', 'NQ']
 fri_37dc_symbols = ['SPX', 'NQ']
 mon_dc_symbols = ['SPX', 'QQQ']
 wed_dc_symbols = ['SPX', 'QQQ']
-wed_dc_symbols = ['QQQ']
 
 adaptive_priority = 'Normal'
 adjust_sleep_interval = 4
