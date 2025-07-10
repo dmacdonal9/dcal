57a1c186faf0d6b14eb2b840fec47c8961ef5835
darcymac <darcymac@gmail.com>
2025-07-10 13:40:03 -0300

    cfg

--- cfg.py
+++ cfg.py
@@ -37,7 +37,7 @@ fri_57dc_params = {
         "long_call_expiry_days": 10,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '20:00:00',  # 4PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
     'QQQ': {
         "quantity": 1,
@@ -57,7 +57,7 @@ fri_57dc_params = {
         "long_call_expiry_days": 10,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '20:00:00',  # 4PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
     'NQ': {
         "quantity": 1,
@@ -77,7 +77,7 @@ fri_57dc_params = {
         "long_call_expiry_days": 10,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '20:00:00',  # 4PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
     'NDX': {
         "quantity": 1,
@@ -97,7 +97,7 @@ fri_57dc_params = {
         "long_call_expiry_days": 10,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '20:00:00',  # 4PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
     'RUT': {
         "quantity": 1,
@@ -117,7 +117,7 @@ fri_57dc_params = {
         "long_call_expiry_days": 21,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '20:00:00',  # 4PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
     'ES': {
         "quantity": 1,
@@ -137,7 +137,7 @@ fri_57dc_params = {
         "long_call_expiry_days": 10,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '20:00:00',  # 4PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
 }
 
@@ -161,7 +161,7 @@ fri_37dc_params = {
         "long_call_expiry_days": 10,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '18:00:00',  # 2PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
     'QQQ': {
         "quantity": 1,
@@ -181,7 +181,7 @@ fri_37dc_params = {
         "long_call_expiry_days": 10,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '18:00:00',  # 2PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
     'NQ': {
         "quantity": 1,
@@ -201,7 +201,7 @@ fri_37dc_params = {
         "long_call_expiry_days": 10,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '18:00:00',  # 2PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
     'NDX': {
         "quantity": 1,
@@ -221,7 +221,7 @@ fri_37dc_params = {
         "long_call_expiry_days": 10,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '18:00:00',  # 2PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
 }
 
@@ -245,7 +245,7 @@ mon_dc_params = {
         "long_call_expiry_days": 7,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '20:00:00',  # 4PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
     'QQQ': {
         "exchange": 'SMART',
@@ -265,7 +265,7 @@ mon_dc_params = {
         "long_call_expiry_days": 7,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '20:00:00',  # 4PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
     'SPX': {
         "exchange": 'CBOE',
@@ -285,7 +285,7 @@ mon_dc_params = {
         "long_call_expiry_days": 7,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '20:00:00',  # 4PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
 }
 
@@ -308,7 +308,7 @@ wed_dc_params = {
         "long_call_expiry_days": 9,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '20:00:00',  # 4PM Atlantic
+        "close_time": '15:00:00',  # 1PM Atlantic
     },
     'QQQ': {
         "exchange": 'SMART',
@@ -328,7 +328,7 @@ wed_dc_params = {
         "long_call_expiry_days": 9,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '20:00:00',  # 4PM Atlantic
+        "close_time": '15:00:00',  # 2PM Atlantic
     },
     'SPX': {
         "exchange": 'CBOE',
@@ -348,6 +348,6 @@ wed_dc_params = {
         "long_call_expiry_days": 9,
         "profit_target_pct": -1,
         "submit_auto_close": True,
-        "close_time": '20:00:00',  # 4PM Atlantic
+        "close_time": '15:00:00',  # 1PM Atlantic
     },
 }
