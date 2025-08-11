ec827a18232b86085a31916b66803e79334f056c
darcymac <darcymac@gmail.com>
2025-08-11 12:36:58 -0300

    fixes

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def open_double_calendar(symbol: str, params: dict, is_live: bool):
             short_call_tickers = fetch_option_chain(und_contract, opt_exchange, short_call_expiry_date, current_mid,
                                                     trading_class=params['trading_class'])
 
-        logger.debug(f"Option chains fetched. Calculating strikes...")
+        logger.debug(f"Option chains fetched. Calculating strikes across {len(short_call_tickers)} tickers")
         short_call_strike = find_option_by_target_delta(short_call_tickers, 'C', params["target_call_delta"],
                                                         trading_class=params['trading_class']).contract.strike
         logger.debug(f"short call found: {short_call_strike}")
